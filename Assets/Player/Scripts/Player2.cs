44e611ce9e6fab6d7f733e92c335389a338bccdb
Adriano Cabral <adrianofelipecabral@gmail.com>
2023-03-06 22:33:28 -0300

    face correct side when stop moving

--- Assets/Player/Scripts/Player2.cs
+++ Assets/Player/Scripts/Player2.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class Player2 : MonoBehaviour
 {
-    public Animator anim;
-    public float speed;
+    private Animator _anim;
+    private Rigidbody2D _body;
+    public float speed = 5f;
+    private Vector2 movement;
 
     // Update is called once per frame
+
+    void Awake()
+    {
+        this._anim = GetComponent<Animator>();
+        this._body = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+        if((movX == 0f && movY == 0f) && (movement.x !=0 || movement.y != 0)){
+            this._anim.SetFloat("LastHorizontal", movement.x);
+            this._anim.SetFloat("LastVertical", movement.y);
+        }
+        movement.x = movX;
+        movement.y = movY;
+        this._anim.SetFloat("Horizontal", movement.x);
+        this._anim.SetFloat("Vertical", movement.y);
+        this._anim.SetFloat("Speed", movement.sqrMagnitude);
+    }
 
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.magnitude);
-
-        transform.position = transform.position + movement * speed * Time.deltaTime;
+    void FixedUpdate()
+    {
+        this._body.MovePosition(this._body.position + movement * speed * Time.deltaTime);
     }
 }
