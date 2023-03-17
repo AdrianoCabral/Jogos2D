e0aeb9172ffda551324c98ea5022e040b1b84b17
Adriano Cabral <adrianofelipecabral@gmail.com>
2023-03-17 02:02:32 -0300

    Save player position

--- Assets/Player/Scripts/Player2.cs
+++ Assets/Player/Scripts/Player2.cs
@@ -20,6 +20,8 @@ public class Player2 : MonoBehaviour
     {
         float movX = Input.GetAxisRaw("Horizontal");
         float movY = Input.GetAxisRaw("Vertical");
+        bool save = Input.GetKeyDown(KeyCode.Alpha1);
+        bool load = Input.GetKeyDown(KeyCode.Alpha2);
         if((movX == 0f && movY == 0f) && (movement.x !=0 || movement.y != 0)){
             this._anim.SetFloat("LastHorizontal", movement.x);
             this._anim.SetFloat("LastVertical", movement.y);
@@ -29,10 +31,29 @@ public class Player2 : MonoBehaviour
         this._anim.SetFloat("Horizontal", movement.x);
         this._anim.SetFloat("Vertical", movement.y);
         this._anim.SetFloat("Speed", movement.sqrMagnitude);
+        if(save){
+            SaveSystem.SavePlayer(this);
+        }
+
+        if(load){
+            LoadPlayer();
+        }
     }
 
     void FixedUpdate()
     {
         this._body.MovePosition(this._body.position + movement * speed * Time.deltaTime);
+    }
+
+    void SavePlayer()
+    {
+        SaveSystem.SavePlayer(this);
+    }
+    
+    void LoadPlayer()
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+        Vector3 position = new Vector3(data.position[0], data.position[1], data.position[2]);
+        transform.position = position;
     }
 }
